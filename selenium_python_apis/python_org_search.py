5ed9e7814c57ce802ef251cb59e7623418affad3
doer <499361328@qq.com>
2019-04-12 17:34:04 +0800

    selenium getting started

---
+++ selenium_python_apis/python_org_search.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+# import sys
+# sys.path.append('C:/Users/doer/dev/tools')
+# 把 chromedriver.exe 放到 C:\Users\doer\Anaconda3\envs\python_dev 或其它位于sys.path中的路径
+
+driver = webdriver.Chrome()
+driver.get("http://www.python.org")
+assert "Python" in driver.title
+elem = driver.find_element_by_name("q")
+elem.clear()
+elem.send_keys("pycon")
+elem.send_keys(Keys.RETURN)
+assert "No results found." not in driver.page_source
+driver.close()
